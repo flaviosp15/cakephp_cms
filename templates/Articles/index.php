b20c79a428bff53a5caa15148dcaedc660718261
flaviosp15 <flaviosp15@outlook.com>
2025-06-01 23:57:03 -0300

    feat: add Articles folder with index file in templates

---
+++ templates/Articles/index.php
@@ -0,0 +1,20 @@
+<h1>Articles</h1>
+<table>
+    <tr>
+        <th>Title</th>
+        <th>Created</th>
+    </tr>
+
+    <!-- Here is where we iterate through our $articles query object, printing out article info -->
+
+    <?php foreach ($articles as $article) { ?>
+    <tr>
+        <td>
+            <?php echo $this->Html->link($article->title, ['action' => 'view', $article->slug]); ?>
+        </td>
+        <td>
+            <?php echo $article->created->format(DATE_RFC850); ?>
+        </td>
+    </tr>
+    <?php } ?>
+</table>
