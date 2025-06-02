c575951dbca6f6af7c48afb24ddc40a0817bfff0
flaviosp15 <flaviosp15@outlook.com>
2025-06-02 02:30:03 -0300

    feat: add update validation rules for articles

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -5,6 +5,7 @@ namespace App\Model\Table;
 use Cake\Event\EventInterface;
 use Cake\ORM\Table;
 use Cake\Utility\Text;
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table
 {
@@ -20,5 +21,18 @@ class ArticlesTable extends Table
             // trim slug to maximum length defined in schema
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+
+        return $validator;
     }
 }
